9b89079d549c7e1ad839a09ad3129ff0e8b707a0
Ryan Vu <ryanvu2003@gmail.com>
2024-07-03 13:20:25 -0400

    Removed clutter and extra animations

--- components/experience/experience.jsx
+++ components/experience/experience.jsx
@@ -1,7 +1,5 @@
 import { Poppins, IBM_Plex_Mono } from "next/font/google";
-import { motion, useInView, useTransform, useScroll } from "framer-motion";
-import { useRef } from "react";
-import Image from "next/image";
+import { motion } from "framer-motion";
 
 const ibm = IBM_Plex_Mono({ subsets: ["latin"], weight: ["400", "700"] });
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
@@ -30,22 +28,16 @@ const experiences = [
 ];
 
 export const Experience = () => {
-  const { scrollYProgress } = useScroll();
-  const height = useTransform(scrollYProgress, [0, 1], ["0vh", "50vh"]);
-
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
-
   return (
-    <section className="h-[50vh] w-full relative border-t-[1px] border-black">
-      <div ref={ref} className="grid grid-rows-3 h-full w-full">
+    <section className="h-[50vh] flex flex-col w-full relative p-4 border-t-[1px] border-black">
+      <div className="grid grid-rows-3 absolute top-0 left-0 h-full w-full">
         {experiences.map((experience, index) => {
           return (
-            <Div index={index + 1} key={index} isInView={isInView}>
-              { 
-                experience.isTitle ? 
-                <h1 className="text-[8vw] px-4" style={ibm.style}>Work</h1> : 
-                <Card title={experience.title} date={experience.date} description={experience.description} location={experience.location} />
+            <Div index={index + 1} key={index}>
+              {
+                experience.isTitle ?
+                  <h1 className="text-[8vw] px-4" style={ibm.style}>Work</h1> :
+                  <Card title={experience.title} date={experience.date} description={experience.description} location={experience.location} />
               }
             </Div>
           )
@@ -55,10 +47,10 @@ export const Experience = () => {
   );
 };
 
-const Div = ({ children, isInView, index }) => {
+const Div = ({ children, index }) => {
   const anim = {
-    initial: { width: "0%" },
-    animate: (i) => (isInView && { width: "100%", transition: { duration: 1, delay: 0.5 * i } }),
+    initial: { opacity: 0, x: "-100%" },
+    animate: (i) => ({ opacity: 1, x: "0%", transition: { duration: 1, delay: 0.5 * i } }),
   };
 
   return (
@@ -77,33 +69,28 @@ const Div = ({ children, isInView, index }) => {
 const Card = ({ title, date, description, src, location }) => {
 
   return (
-    <motion.div
-      whileHover={{ width: "80%", transition: { duration: 0.5 } }}
-      className="flex flex-col w-full"
-    >
-      <div className="flex flex-col">
-        <div
-          className="flex items-center gap-2 p-2 uppercase border-b-[1px] border-black bg-black text-white"
-          style={ibm.style}
-        >
-          <motion.div
-            animate={{ rotate: [0, 360] }}
-            transition={{
-              duration: 3,
-              repeat: Infinity,
-              repeatType: "mirror",
-              ease: "linear",
-            }}
-            className="h-2 w-2 bg-white rotate-45"
-          ></motion.div>
-          <span>{date}</span>
-        </div>
-        <h2 className={`${poppins.className} text-4xl md:text-6xl p-2 py-4`}>{title}</h2>
-        <div className="flex border-t-[1px] border-black justify-between text-xs p-2">
-          <span>{description}</span>
-          <span>{location}</span>
-        </div>
+    <div className="flex flex-col">
+      <div
+        className="flex items-center gap-2 p-2 uppercase border-b-[1px] border-black bg-black text-white"
+        style={ibm.style}
+      >
+        <motion.div
+          animate={{ rotate: [0, 360] }}
+          transition={{
+            duration: 3,
+            repeat: Infinity,
+            repeatType: "mirror",
+            ease: "linear",
+          }}
+          className="h-2 w-2 bg-white rotate-45"
+        ></motion.div>
+        <span>{date}</span>
       </div>
-    </motion.div>
+      <h2 className={`${poppins.className} text-4xl md:text-6xl p-2 py-4`}>{title}</h2>
+      <div className="flex border-t-[1px] border-black justify-between text-xs p-2">
+        <span>{description}</span>
+        <span>{location}</span>
+      </div>
+    </div>
   );
 };

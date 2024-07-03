91ede7e4cb704525a575c0e6df209534cfd88059
Ryan Vu <ryanvu2003@gmail.com>
2024-07-03 12:19:50 -0400

    expanded suspense

--- app/layout.js
+++ app/layout.js
@@ -15,9 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SmoothScroller />
-        <Header />
         <Suspense>
+          <SmoothScroller />
+          <Header />
           {children}
         </Suspense>
       </body>

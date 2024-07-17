3a8865dc9d0b54b6f92115ca8857d76b242eb054
Ryan Vu <ryanvu2003@gmail.com>
2024-07-17 17:54:38 -0400

    completed projects sections

---
+++ components/projects/project-data.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "meal.",
+    year: '2024', desc: 'Meal plan generator written in Elixir, Phoenix LiveView',
+    work: 'Plan, Design, Code / Learning Project',
+    imgs: [
+      { src: '/images/projects/meal/meal-1.png', alt: 'Meal Logo' },
+      { src: '/images/projects/meal/meal-2.png', alt: 'Meal Landing Page' },
+      { src: '/images/projects/meal/meal-3.png', alt: 'Meal Screenshot' }
+    ]
+  },
+  {
+    title: "Capital Golf Exchange",
+    year: '2022',
+    desc: 'Contracted to create admin features requested by business owner in Vue.js + Firebase',
+    work: 'Plan, Code, Upgrade',
+    imgs: [
+      { src: '/images/projects/cge/cge-1.png', alt: 'CGE Logo' },
+      { src: '/images/projects/cge/cge-2.png', alt: 'CGE Landing Page' },
+      { src: '/images/projects/cge/cge-3.png', alt: 'CGE Landing Page 2' }
+    ]
+  },
+  {
+    title: "Natalie Mitchell Music",
+    year: '2021',
+    desc: 'Portfolio for Natalie Mitchell Music written with Next.js',
+    work: 'Design, Code, and Deploy',
+    imgs: [
+      { src: '/images/projects/natmusic/natmusic-1.png', alt: 'Opening Landing Page Natalie Music' },
+      { src: '/images/projects/natmusic/natmusic-2.png', alt: 'Home Page Natalie Music' },
+      { src: '/images/projects/natmusic/natmusic-3.png', alt: 'Natalie Music - Mobile Contact' }
+    ]
+  },
+  // {
+  //   title: "Learn Kanji",
+  //   year: '2021',
+  //   imgs: []
+  // },
+  {
+    title: "Deluxe Time",
+    year: '2019',
+    desc: 'Landing page for small watch-repair business written with React.js',
+    work: 'Design, Code, and Deploy',
+    imgs: [
+      { src: '/images/projects/deluxe/deluxe-1.png', alt: 'Deluxe Time Logo' },
+      { src: '/images/projects/deluxe/deluxe-2.png', alt: 'Deluxe Time HomePage' }
+    ]
+  },
+];
--- components/projects/projects.jsx
+++ components/projects/projects.jsx
@@ -1,32 +1,43 @@
-import { IBM_Plex_Mono } from "next/font/google";
+import { IBM_Plex_Mono, Barlow } from "next/font/google";
 import { useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
+import { projects } from "./project-data";
 import Image from "next/image";
 
-const projects = [
-  { title: "meal.", year: '2024', imgs: [{ src: '/images/projects/meal/meal-1.png', alt: '' }, { src: '/images/projects/meal/meal-2.png', alt: '' }, { src: '/images/projects/meal/meal-3.png', alt: '' }] },
-  { title: "Capital Golf Exchange", year: '2022', imgs: [] },
-  { title: "Natalie Mitchell Music", year: '2021', imgs: [] },
-  { title: "learn kanji", year: '2021', imgs: [] },
-  { title: "deluxe time", year: '2019', imgs: [{ src: '/images/projects/deluxe/deluxe-1.png', alt: '' }] },
-]
 
 const ibm = IBM_Plex_Mono({ subsets: ["latin"], weight: ["400", "500"] });
+const barlow = Barlow({ subsets: ["latin"], weight: ["400", "500"] });
 
 export const Projects = () => {
   const [selectedIndex, setSelectedIndex] = useState(null);
+  const [selectedImgIndex, setSelectedImgIndex] = useState(0);
 
   const targetRef = useRef(null);
+
+  const handleImgChange = (index) => {
+    setSelectedImgIndex(index);
+  }
+
   return (
     <section ref={targetRef} className="relative flex flex-col gap-12 w-full p-4 lg:p-8">
       {/* <InfiniteText text="Projects  Projects  Projects  Projects  Projects" /> */}
       <h1 className="text-[4vw] leading-10 ml-auto uppercase" style={ibm.style}>Projects</h1>
-      <div className="flex gap-4">
-        <div className="hidden lg:flex h-[60vh] w-[40vw] border-black border-[1px]">
+      <div className="flex gap-12">
+        <div style={barlow.style} className="hidden lg:flex flex-col gap-2 items-center h-[50vh] w-[40vw]">
           {selectedIndex !== null && (
-            <div>Displaying details for project {projects[selectedIndex].title}</div>
+            <ProjectImg
+              isMain
+              alt={projects[selectedIndex].imgs[selectedImgIndex]?.alt}
+              src={projects[selectedIndex].imgs[selectedImgIndex]?.src} />
           )}
+
+          <div className="grid grid-cols-3 w-full">
+            <h2>{projects[selectedIndex]?.title}</h2>
+            <h2 className="col-end-9">{projects[selectedIndex]?.year}</h2>
+          </div>
+          <h3 className="self-start uppercase text-xs">{projects[selectedIndex]?.desc}</h3>
+          <h3 className="self-start uppercase text-xs">{projects[selectedIndex]?.work}</h3>
         </div>
         <div className="flex flex-col flex-grow">
           {
@@ -38,6 +49,7 @@ export const Projects = () => {
                   key={i}
                   isSelected={selectedIndex === i}
                   onSelected={() => setSelectedIndex(i)}
+                  onImgChange={handleImgChange}
                 />
               )
             })
@@ -48,13 +60,25 @@ export const Projects = () => {
   );
 };
 
-const ProjectDiv = ({ project, index, isSelected, onSelected }) => {
+const ProjectDiv = ({ project, index, isSelected, onSelected, onImgChange }) => {
+  const [selectedImgIndex, setSelectedImgIndex] = useState(0);
+
+  const handleProjectClick = () => {
+    onSelected();
+    setSelectedImgIndex(0);
+    onImgChange(0);
+  }
+
+  const handleImgClick = (i) => {
+    setSelectedImgIndex(i)
+    onImgChange(i);
+  }
 
   return (
     <>
       <motion.div
         className="grid grid-cols-3 text-lg border-b-[1px] p-2 border-black"
-        onClick={onSelected}
+        onClick={handleProjectClick}
         whileHover={{ opacity: 0.4 }}
       >
         <span style={ibm.style}>RV-00{index + 1}</span>
@@ -63,14 +87,26 @@ const ProjectDiv = ({ project, index, isSelected, onSelected }) => {
       </motion.div>
       {isSelected &&
         <AnimatePresence mode="popLayout">
-          <motion.div className="flex gap-1 m-2" key={isSelected} initial={{ height: 0 }} animate={{ height: 180, transition: { duration: 0.2 } }} exit={{ height: 0, transition: { duration: 0.2 } }}>
-            {project.imgs.map((img, i) => { 
-              return <div key={i} className="border-current border relative aspect-video w-60 h-44">
-                <Image fill src={img.src} alt={img.alt} className="object-cover" />
-              </div>
-            })}
+          <motion.div className="flex gap-4 my-4" key={isSelected} initial={{ height: 0 }} animate={{ height: 180, transition: { duration: 0.2 } }} exit={{ height: 0, transition: { duration: 0.2 } }}>
+            {project.imgs.map((img, i) =>
+              <ProjectImg key={i} src={img.src} alt={img.alt} isSelectedImg={selectedImgIndex === i} onSelectedImg={() => handleImgClick(i)} />
+            )}
           </motion.div>
         </AnimatePresence>}
     </>
+  )
+}
+
+const ProjectImg = ({ src, alt, isSelectedImg, onSelectedImg, isMain }) => {
+  const size = isMain ? 'w-3/4 h-3/4' : 'w-60 h-44';
+  const scale = isMain ? 'object-contain' : 'object-scale-down';
+  const border = isMain ? null : isSelectedImg ? 'border-2 border-current' : 'border border-current';
+  const className = `${border} relative aspect-video ${size}`;
+  return (
+    <div
+      onClick={onSelectedImg}
+      className={className}>
+      <Image fill src={src} alt={alt} className={scale} />
+    </div>
   )
 }
